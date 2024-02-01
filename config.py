cd6261385a79c5a5f49da00926b1c7da44a196fc
Erik Westlund <erikwestlund@hey.com>
2024-02-01 15:11:59 -0500

    fix install

--- config.py
+++ config.py
@@ -207,7 +207,7 @@ def run_set_config(self, update=False):
 
     # Set JHED if not set or update is True
     jhed_username_default = config["credentials"]["jhed"]["username"] or get_default_jhed()
-    
+
     if first_run or update or not config["credentials"]["jhed"]["username"]:
         config["credentials"]["jhed"]["username"] = click.prompt(
             "Enter your JHED (without @jh.edu)",
