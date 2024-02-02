9efe58c86cde46bb174b88ade4b2b5a768f6a301
Erik Westlund <erikwestlund@hey.com>
2024-02-02 14:12:33 -0500

    more r data science packages

--- resources/install/r-data-science.sh
+++ resources/install/r-data-science.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
+# Development tools and system dependencies
 Rscript -e "install.packages('devtools', repos='https://cran.rstudio.com/')"
 Rscript -e "install.packages('remotes', repos='https://cran.rstudio.com/')"
-Rscript -e "install.packages('tidyverse', repos='https://cran.rstudio.com/')"
-Rscript -e "install.packages('renv', repos='https://cran.rstudio.com/')"
-Rscript -e "install.packages('DBI', repos='https://cran.rstudio.com/')"
 Rscript -e "install.packages('rJava', repos='https://cran.rstudio.com/')"
+Rscript -e "install.packages('renv', repos='https://cran.rstudio.com/')"
+Rscript -e "install.packages('rlang', repos='https://cran.rstudio.com/')"
+Rscript -e "install.packages('knitr', repos='https://cran.rstudio.com/')"
+Rscript -e "install.packages('openssl', repos='https://cran.rstudio.com/')"
+
+# Tidyverse:.  Core packages for data science, includes:
+# ggplot2, dplyr, tidyr, readr, purrr, tibble, stringr, forcats
+Rscript -e "install.packages('tidyverse', repos='https://cran.rstudio.com/')"
+
+# Database connection packages and utilities
+Rscript -e "install.packages('DBI', repos='https://cran.rstudio.com/')"
 Rscript -e "install.packages('RPostgres', repos='https://cran.rstudio.com/')"
 Rscript -e "install.packages('RSQLite', repos='https://cran.rstudio.com/')"
 Rscript -e "install.packages('odbc', repos='https://cran.rstudio.com/')"
